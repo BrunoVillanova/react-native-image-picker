4e423360c304e21ada40825111e5090ef9b74e9d
iyegoroff <iegoroff@gmail.com>
2022-11-28 01:56:16 +0300

    ts fixes

--- src/platforms/NativeImagePicker.ts
+++ src/platforms/NativeImagePicker.ts
@@ -1,10 +1,7 @@
-import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
-import { TurboModuleRegistry } from 'react-native';
+import {TurboModuleRegistry, TurboModule} from 'react-native';
 
 export interface Spec extends TurboModule {
   launchCamera(options: Object, callback: () => void): void;
   launchImageLibrary(options: Object, callback: () => void): void;
 }
-export default TurboModuleRegistry.get<Spec>(
-  'ImagePicker'
-);
+export default TurboModuleRegistry.get<Spec>('ImagePicker');
